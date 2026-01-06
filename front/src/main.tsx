bb909c4b5f395641b1aa933b8431a44c63d40c2b
H2oo1 <hfoort29@gmail.com>
2026-01-06 14:58:32 +0100

    First commit of challenges page ==> Purely visual nothing is connected

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Challenges from "./pages/Challenges.tsx";
+import Challenges from "./pages/Challenges/Challenges.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Ranking from "./pages/Ranking.tsx";
@@ -12,14 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route>
           <Route path="/" element={<Navigate to="/challenges" replace />} />
           <Route path="/challenges" element={<Challenges />} />
           <Route path="/challenges/:id" element={<ChallengeDetail />} />
           <Route path="/ranking" element={<Ranking />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

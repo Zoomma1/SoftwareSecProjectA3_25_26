3e77345d09bdc751509423e7f9a1000f574279c1
Sophie Ton <sophieton03@gmail.com>
2026-01-06 13:51:32 +0100

    Fix sidebar navigation and add icons

--- front/src/pages/Challenges.tsx
+++ front/src/pages/Challenges.tsx
@@ -1,3 +1,12 @@
+import Sidebar from "../components/Sidebar/Sidebar";
+
 export default function Challenges() {
-  return <div>Challenges page</div>;
+  return (
+    <>
+      <Sidebar />
+      <div className="test">
+        <h1>Challenge</h1>
+      </div>
+    </>
+  );
 }

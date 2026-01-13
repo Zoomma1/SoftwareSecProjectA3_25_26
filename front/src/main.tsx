d2489be0fec9706280dca5295f75dc205d3795e1
H2oo1 <hfoort29@gmail.com>
2026-01-13 13:13:26 +0100

    Creating Button and Modal components

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -20,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
               <Route path="/challenges" element={<Challenges />} />
               <Route path="/challenges/:id" element={<ChallengeDetail />} />
               <Route path="/ranking" element={<Ranking />} />
+              <Route path="/profile" element={<Profile  />} />
             </Route>
           <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
